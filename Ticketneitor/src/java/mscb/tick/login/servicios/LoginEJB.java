6c1e48b6c8791f7be1f13c2d4a0345732eb1dc7f
MatthGitHub <matiasbenditti@hotmail.com>
2016-09-27 15:41:51 -0300

    Arreglo mensajes de login, ahora son correctos - V1.1.1

--- Ticketneitor/src/java/mscb/tick/login/servicios/LoginEJB.java
+++ Ticketneitor/src/java/mscb/tick/login/servicios/LoginEJB.java
@@ -9,6 +9,7 @@ import java.util.List;
 import mscb.tick.entidades.Usuarios;
 import mscb.tick.seguridad.MD5;
 import mscb.tick.usuarios.servicios.UsuarioServ;
+
 /**
  *
  * @author Matth
@@ -28,27 +29,29 @@ public class LoginEJB {
     public boolean login(String nombre, String clave) {
         md5 = new MD5();
         clave = md5.md5(clave);
-        
+
         serviciosU = new UsuarioServ();
         miLista = serviciosU.traerTodos();
         for (int i = 0; i < miLista.size(); i++) {
             if (miLista.get(i).getNombreUsuario().equalsIgnoreCase(nombre)) {
                 if (miLista.get(i).getContrasenia().equals(clave)) {
-                    if (miLista.get(i).getActivo() == true) {
+                    if (miLista.get(i).getActivo()) {
                         usuario = miLista.get(i);
                         return true;
                     } else {
                         mensaje = "Usuario Inactivo";
+                        return false;
                     }
                 } else {
                     mensaje = "Clave incorrecta";
+                    return false;
                 }
-            }else{
+            } else {
                 mensaje = "Usuario inexistente";
+                return false;
             }
-
-            }
-        return false;
-
+            
         }
+    return false;
     }
+}
